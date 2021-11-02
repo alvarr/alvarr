2677fe34e962d800940f04ebc5d505de649dd014
alvarr <alvaro.aguilar@goumh.umh.es>
2021-11-02 12:59:15 +0100

    Primera versión completa

--- Tarea2/Tarea2.c
+++ Tarea2/Tarea2.c
@@ -14,7 +14,14 @@
 
 #define N 1200
 
-/*Reakuza los pasos de la primera iteración*/
+
+/**
+ * @brief Realiza los pasos de la primera iteración
+ * 
+ * @param matriz Matriz que usaremos para las iteraciones
+ * @param v_uni Vector principal
+ * @param v_aux Vector auxiliar
+ */
 void primeraIteracion(double **matriz, double *v_uni, double *v_aux){
 
     double a;
@@ -33,7 +40,15 @@ void primeraIteracion(double **matriz, double *v_uni, double *v_aux){
     }
 }
 
-/*Realiza los pasos de las iteraciones impares*/
+
+/**
+ * @brief Realiza los pasos de las iteraciones impares
+ * 
+ * @param matriz Matriz que usaremos para las iteraciones
+ * @param v_uni Vector principal
+ * @param v_aux Vector auxiliar
+ * @return double 
+ */
 double siguienteIteracion(double **matriz, double *v_uni, double *v_aux){
     
     //Multiplicamos la matriz por el vector resultado de la anterior iteracion y almacenamos el resultado en el vector auxiliar
@@ -69,7 +84,16 @@ double siguienteIteracion(double **matriz, double *v_uni, double *v_aux){
     
 }
 
-/*Realiza según el número de iteración que sea una operación u otra*/
+
+/**
+ * @brief Realiza según el número de iteración que sea una operación u otra
+ * 
+ * @param num_ite Número de iteraciones
+ * @param matriz Matriz que usaremos para las iteraciones
+ * @param v_uni Vector principal
+ * @param v_aux Vector auxiliar
+ * @param v_absoluto Vector que guarda los valores absolutos
+ */
 void realizarIteraciones(int num_ite, double **matriz,double *v_uni,double *v_aux, double *v_absoluto){
     for(int i = 1;i<=num_ite;i++){
         if(i==1){
@@ -81,7 +105,18 @@ void realizarIteraciones(int num_ite, double **matriz,double *v_uni,double *v_au
     
 }
 
-/*Guardar en un fichero información sobre los parámetros de entrada, normas calculadas, norma vector final, tiempos de ejecución, guardado y generado de matriz y total*/
+/**
+ * @brief Guardar en un fichero información sobre los parámetros de entrada, normas calculadas, norma vector final, tiempos de ejecución, guardado y generado de matriz y total
+ * 
+ * @param nom_fich_resm Nombre del fichero que guarda todos los datos de la ejecución del programa
+ * @param nom_fich_matr Nombre del fichero que contiene la matriz
+ * @param num_iteraciones Número de iteraciones que realiza el programa
+ * @param t_gen Tiempo que tarda en generarse la matriz
+ * @param t_guar Tiempo que tarda en guardarse la matriz
+ * @param t_ejec Tiempo que tarda en ejecutarse las iteraciones
+ * @param t_global Tiempo global que dura toda la ejecución del programa
+ * @param v_absoluto Vector que contiene el valor absoluto de cada iteración
+ */
 void guardarResumen(char nom_fich_resm[],char nom_fich_matr[],  int num_iteraciones, double t_gen, double t_guar, double t_ejec, double t_global,double *v_absoluto){
     FILE *f1;
     f1 = fopen(nom_fich_resm,"w");
@@ -95,6 +130,7 @@ void guardarResumen(char nom_fich_resm[],char nom_fich_matr[],  int num_iteracio
 
     
     fprintf(f1,"Valores absolutos: \n");
+    fprintf(f1,"Iteracion 0: No se aplica \n");
     for(int i = 0;(i<num_iteraciones-1);i++){
         fprintf(f1,"Iteracion %d: %.3lf \n",(i+1), v_absoluto[i]);
     }
@@ -107,7 +143,19 @@ void guardarResumen(char nom_fich_resm[],char nom_fich_matr[],  int num_iteracio
     fclose(f1);
 }
 
-/*Imprime información sobre los parámetros de entrada, normas calculadas, norma vector final, tiempos de ejecución, guardado y generado de matriz y total*/
+
+/**
+ * @brief Imprime información sobre los parámetros de entrada, normas calculadas, norma vector final, tiempos de ejecución, guardado y generado de matriz y total
+ * 
+ * @param num_iteraciones Número de iteraciones que realiza el programa
+ * @param nom_fich_sal Nombre del fichero que guarda todos los datos de la ejecución del programa
+ * @param nom_fich_matr Nombre del fichero que contiene la matriz
+ * @param t_gen Tiempo que tarda en generarse la matriz
+ * @param t_guar Tiempo que tarda en guardarse la matriz
+ * @param t_ejec Tiempo que tarda en ejecutarse las iteraciones
+ * @param t_global Tiempo global que dura toda la ejecución del programa
+ * @param v_absoluto Vector que contiene el valor absoluto de cada iteración
+ */
 void imprimirResumen(int num_iteraciones, char *nom_fich_sal, char *nom_fich_matr, double t_gen, double t_guar, double t_ejec, double t_global, double *v_absoluto){
 
     printf("Numero de iteraciones: %i \n", num_iteraciones);
@@ -115,6 +163,7 @@ void imprimirResumen(int num_iteraciones, char *nom_fich_sal, char *nom_fich_mat
     printf("Nombre fichero matriz: %s \n", nom_fich_matr);
 
     printf("Valores absolutos: \n");
+    printf("Iteracion 0: No se aplica \n");
     for(int i = 0;i<(num_iteraciones-1);i++){
         printf("Iteracion %d: %.3lf \n",(i+1), v_absoluto[i]);
     }
@@ -124,7 +173,13 @@ void imprimirResumen(int num_iteraciones, char *nom_fich_sal, char *nom_fich_mat
     printf("Tiempo(milisengundos) global:  %f \n", t_global);
 }
 
-/*Guarda en un fichero la matriz*/
+
+/**
+ * @brief Guarda en un fichero la matriz
+ * 
+ * @param matriz matriz que contiene la matriz que debemos de guardar
+ * @param nom_fich_matr Nombre del fichero donde guardaremos la matriz
+ */
 void guardarMatriz(double **matriz, char *nom_fich_matr ){
     FILE *f1;
     f1 = fopen(nom_fich_matr,"wb");
@@ -135,31 +190,41 @@ void guardarMatriz(double **matriz, char *nom_fich_matr ){
     fclose(f1);
 }
 
-/*Genera una matriz de tamaño NxN*/
+
+/**
+ * @brief Genera una matriz de tamaño NxN
+ * 
+ * @param matriz matriz donde se guardará la matriz que generamos
+ */
 void generarMatriz(double **matriz){
     double aux,a=0;
     srand(time(NULL));
-    //srand(getpid());
+    
     for(int x =0; x<N;x++){
         for(int y =0; y<N;y++){
             
             if(x == y){ //Elementos de la diagonal
                 matriz[x][y] = 1;
             }else if(x < y){ //Elementos triangular inferior
-                aux = rand( ) % 50000 + 1;
-                matriz[x][y] = (double)aux/1000;
+                aux = rand( ) % 50001;
+                matriz[x][y] = (double)50*(x+1)*(y+1)/(N+N);
               
-            }else{//Elementos triangular superior
-                aux = -1*(rand( ) % 50000 + 1);
-                matriz[x][y] = (double)aux/1000;
+            }else{//Elementos triangular superior valores negativos
+                aux = -1*(rand( ) % 50001);
+                matriz[x][y] = (double)-50*(x+1)*(y+1)/(N+N); 
             }
-           // printf("%.1lf ", matriz[x][y]);
+            //printf("%.1lf ", matriz[x][y]);
         }
        // printf("\n");
     }
 }
 
-/*Lee los datos de un fichero para rellenar una matriz*/
+/**
+ * @brief Lee los datos de un fichero para rellenar una matriz
+ * 
+ * @param matriz Matriz que almacenará la matriz guardada en elfichero
+ * @param nom_fich_matr Nombre del fichero que contiene la matriz
+ */
 void leerFichero(double **matriz,char *nom_fich_matr){
     FILE *f1;
     f1 = fopen(nom_fich_matr,"rb");
@@ -170,16 +235,28 @@ void leerFichero(double **matriz,char *nom_fich_matr){
     fclose(f1);
 }
 
-
+/**
+ * @brief Función principal
+ * 
+ * @param argc Número de parametros que pasamos al ejecutar el programa
+ * @param argv Parametros que pasamos al ejecutar el programa
+ * @return int 
+ */
 int main(int argc, char *argv[]){
 
     //Declaramos las variables de tiempo e iniciamos el contador del tiempo total
     clock_t t_ini, t_fin, t_init, t_fint;
     t_init = clock();
 
+    //Indicamos un parámetro de entrada que explique que información hay que pasar como parametro
+    if(argc == 2 && strcmp("HELP",argv[1])==0){
+        printf("Tienes que indicar el numero de iteraciones, el fichero de salida para los datos y el fichero de entrada para la matriz como argumentos, respectivamente\n");
+        return 0;
+    }
+
     //Si no recibimos los dos parametros la ejecución se para
     if(argc != 4){
-        printf("Tienes que indicar el número de iteraciones, el fichero de salida para los datos y el fichero de entrada para la matriz como argumentos, respectivamente\n");
+        printf("Debes indicar varios paramentros. Para mas ayuda pasa como parametro HELP\n");
         return 0;
     }
 
